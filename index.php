2befe031de8de36dd27a5ded0fa457d446790758
Zak Thompson <zakthompson@ZbookPRO.lan>
2017-08-29 15:34:20 -0400

    testing!

--- index.php
+++ index.php
@@ -2,19 +2,26 @@
 
 require_once __DIR__ . "/vendor/autoload.php";
 
-$collection = new MongoDB\Client("mongodb://172.16.0.85:27017/");
+$mongo = new MongoDB\Client("mongodb://172.16.0.85:27017/");
+
+$collection = $mongo->MarketCollector->market_data;
+$cursor2 = $collection->find($query);
 
 $pair = $_GET["pair"];
 $exchange = $_GET["exchange"];
 $periods = $_GET["periods"];
 
-$query = 'db.market_data.find({
+
+$query = '({
     "exchange": "'.$exchange.'",
     "pair": "'.$pair.'"
 }).sort({
     "CloseTime": -1
 }).limit('.$periods.');';
 
-echo $query;
+$cursor2 = $collection->find($query);
+
+print_r($cursor2);
+
 
 ?>
