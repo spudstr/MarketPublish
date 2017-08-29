a9edf7b2f1d28f5bc402251d76b1001bedc2e21a
Zak Thompson <zakthompson@ZbookPRO.lan>
2017-08-29 17:14:49 -0400

    debug

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ require_once __DIR__ . "/vendor/autoload.php";
 $mongo = new MongoDB\Client("mongodb://172.16.0.85:27017/");
 
 $collection = $mongo->MarketCollector->market_data;
-$cursor2 = $collection->find($query);
+
 
 $pair = $_GET["pair"];
 $exchange = $_GET["exchange"];
