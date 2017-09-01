5d5b71c9e74bd5f646b43c0543ca38f82acfe4ae
Zak Thompson <zakthompson@ZbookPRO.local>
2017-09-01 10:46:03 -0400

    fairV

---
+++ fairV.php
@@ -0,0 +1,118 @@
+<?php
+
+require_once __DIR__ . "/vendor/autoload.php";
+
+//$manager = new MongoDB\Driver\Manager("mongodb://172.16.0.85:27017/");
+
+
+$pair = $_GET["pair"];
+$exchange = $_GET["exchange"];
+$periods = $_GET["periods"];
+
+
+
+$aggregateQuery = '[
+// Stage 1
+{
+  $match: {
+    pair: \'LTCUSD\'
+  }
+},
+// Stage 2
+{
+  $group: {
+    "_id": {
+      "year": {
+        "$year": "$timestamp"
+      },
+      "month": {
+        "$month": "$timestamp"
+      },
+      "day": {
+        "$dayOfMonth": "$timestamp"
+      },
+      "day2": {
+        "$dayOfYear": "$timestamp"
+      },
+      "hour": {
+        "$hour": "$timestamp"
+      },
+      "minute_interval": {
+        "$subtract": [{
+            "$minute": "$timestamp"
+          },
+          {
+            "$mod": [{
+              "$minute": "$timestamp"
+            }, 1]
+          }
+        ]
+      }
+    },
+    "TopAsk": {
+      "$min": "$ASK"
+    },
+    "TopBid": {
+      "$max": "$BID"
+    },
+    "timestamp": {
+      "$first": "$timestamp"
+    }
+  }
+},
+// Stage 3
+{
+  $project: {
+    _id: 0,
+    TopAsk: 1,
+    TopBid: 1,
+    ASK: 1,
+    BID: 1,
+    FairV: {
+      $divide: [{
+        $add: ["$TopAsk", "$TopBid"]
+      }, 2]
+    },
+    timestamp: 1,
+    tz: 1
+  }
+},
+// Stage 4
+{
+  $sort: {
+    timestamp: -1
+  }
+},
+// Stage 5
+{
+  $limit: 500
+},
+]';
+
+echo $aggregateQuery . "<br>";
+die;
+
+$filter = [['excange' => 'bitfinex', 'pair' => 'ltcusd'], ['CloseTime' => 1, 'ClosePrice' => 1]];
+
+$filter = array(
+    "excange" => $exchange,
+    "pair" => $pair
+);
+$options = array(
+    "sort" => array(
+        "CloseTime" => -1,
+    )
+
+);
+
+$readPreference = new MongoDB\Driver\ReadPreference(MongoDB\Driver\ReadPreference::RP_PRIMARY);
+$query = new MongoDB\Driver\Query($filter, $options);
+$manager = new MongoDB\Driver\Manager("mongodb://127.0.0.1:27017/");
+$result = $manager->executeQuery("MarketCollector.market_data", $query, $readPreference);
+
+var_dump($result);
+
+
+
+
+?>
