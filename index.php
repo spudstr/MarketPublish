eb3a117cba51535c8deb78d98e76099b8bd790f0
Zak Thompson <zakthompson@ZbookPRO.local>
2017-09-01 15:28:44 -0400

    wef

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once __DIR__ . "/vendor/autoload.php";
-
+header('Content-Type: application/json');
 //$manager = new MongoDB\Driver\Manager("mongodb://172.16.0.85:27017/");
 
 
@@ -30,6 +30,7 @@ $filter = array(
     "pair" => $pair
 );
 $options = array(
+
     "sort" => array(
         "CloseTime" => -1,
     )
