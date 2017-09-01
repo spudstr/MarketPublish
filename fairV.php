1fca27c992d0cf15369880ff039866bd24d22128
Zak Thompson <zakthompson@ZbookPRO.local>
2017-09-01 18:28:20 -0400

    final

--- fairV.php
+++ fairV.php
@@ -1,12 +1,12 @@
 <?php
 
 require_once __DIR__ . "/vendor/autoload.php";
-
+header('Content-Type: application/json');
 //$manager = new MongoDB\Driver\Manager("mongodb://172.16.0.85:27017/");
 
 
-$pair = $_GET["pair"];
-$exchange = $_GET["exchange"];
+$pair = strtoupper($_GET["pair"]);
+$exchange = strtoupper($_GET["exchange"]);
 $periods = $_GET["periods"];
 
 
@@ -33,28 +33,31 @@ $manager = new MongoDB\Driver\Manager("mongodb://127.0.0.1:27017/");
 $query = new MongoDB\Driver\Command([
 'aggregate' => 'bitfinex_ticker',
  'pipeline' => [
-    ['$match' => ['pair' => 'LTCUSD']],
+    ['$match' => ['pair' => $pair]],
     ['$group' => ['_id' => ['year' => ['$year' => '$timestamp'],'month' => ['$month' => '$timestamp'],'day' => ['$dayOfMonth' => '$timestamp'],'day2' => ['$dayOfYear' => '$timestamp'],'hour' => ['$hour' => '$timestamp'],'minute_interval' => ['$subtract' => [['$minute' => '$timestamp'],['$mod' => [['$minute' => '$timestamp'], 1]]]]],'TopAsk' => ['$min' => '$ASK'],'TopBid' => ['$max' => '$BID'],'timestamp' => ['$first' => '$timestamp']]],
     ['$project' => ['_id' => 0,'TopAsk' => 1,'TopBid' => 1,'ASK' => 1,'BID' => 1,'FairV' => ['$divide' => [['$add' => ['$TopAsk', '$TopBid']], 2]],'timestamp' => 1,'tz' => 1]],
     ['$sort' => ['timestamp' => -1]],
-    ['$limit' => 500]
+    ['$limit' => (int)$periods]
   ],
   'cursor' => new stdClass,
 ]);
 $cursor = $manager->executeCommand('MarketCollector',$query);
 
 $final = array();
-$dataArray[] = array();
+$dataArray = array();
 
   foreach ($cursor as $data)
   {
-         $dataArray["result"][$periods][] = array($data->TopAsk,$data->TopBid,$data->FairV);
-         $temp_array = $data->timestamp;
-         var_dump($temp_array);
-         echo "<br>";
+
+         $tmpObject = $data->timestamp;
+          $ts = get_object_vars($tmpObject);
+
+         $dataArray["result"][$periods][] = array('TopAsk' => $data->TopAsk,'TopBid' => $data->TopBid,"FairValue" => $data->FairV,'Timestamp' => (int)$ts["milliseconds"]);
+
+
   }
 
 
-//echo json_encode($final,JSON_PRETTY_PRINT);
+echo json_encode($dataArray,JSON_PRETTY_PRINT);
 
 ?>
