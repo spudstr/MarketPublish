013b42e1adc3de2d0b2083d3ff3e9985f7c165ac
Zak Thompson <zakthompson@ZbookPRO.local>
2017-09-01 14:57:20 -0400

    wefef

--- fairV.php
+++ fairV.php
@@ -11,81 +11,7 @@ $periods = $_GET["periods"];
 
 
 
-$aggregateQuery = '
-["aggregate" => "bitfinex_ticker"],
-"pipeline" =>
-  [
-    [
-      $match => [
-        pair => "LTCUSD"
-      ]
-    ],
-    [
-      $group => [
-        "_id" => [
-          "year" => [
-            "$year" => "$timestamp"
-          ],
-          "month" => [
-            "$month" => "$timestamp"
-          ],
-          "day" => [
-            "$dayOfMonth" => "$timestamp"
-          ],
-          "day2" => [
-            "$dayOfYear" => "$timestamp"
-          ],
-          "hour" => [
-            "$hour" => "$timestamp"
-          ],
-          "minute_interval" => [
-            "$subtract" => [[
-                "$minute" => "$timestamp"
-              ],
-              [
-                "$mod" => [[
-                  "$minute" => "$timestamp"
-                ], 1]
-              ]
-            ]
-          ]
-        ],
-        "TopAsk" => [
-          "$min" => "$ASK"
-        ],
-        "TopBid" => [
-          "$max" => "$BID"
-        ],
-        "timestamp" => [
-          "$first" => "$timestamp"
-        ]
-      ]
-    ],
-    [
-      $project => [
-        _id => 0,
-        TopAsk => 1,
-        TopBid => 1,
-        ASK => 1,
-        BID => 1,
-        FairV => [
-          $divide => [[
-            $add => ["$TopAsk", "$TopBid"]
-          ], 2]
-        ],
-        timestamp => 1,
-        tz => 1
-      ]
-    ],
-    [
-      $sort => [
-        timestamp => -1
-      ]
-    ],
-    [
-      $limit => 500
-    ],
-  ]';
+$aggregateQuery = '';
 
 
 //print_r($aggregateQuery);
@@ -104,7 +30,16 @@ $result = $manager->executeQuery("MarketCollector.bitfinex_ticker", $query, $rea
 $readPreference = new MongoDB\Driver\ReadPreference(MongoDB\Driver\ReadPreference::RP_PRIMARY);
 //$query = new MongoDB\Driver\Query($filter, $options);
 $manager = new MongoDB\Driver\Manager("mongodb://127.0.0.1:27017/");
-$query = new MongoDB\Driver\Command($aggregateQuery);
+$query = new MongoDB\Driver\Command([
+'aggregate' => 'bitfinex_ticker',
+ 'pipeline' => [
+    ['$match' => ['pair' => 'LTCUSD']],
+    ['$group' => ['_id' => ['year' => ['$year' => '$timestamp'],'month' => ['$month' => '$timestamp'],'day' => ['$dayOfMonth' => '$timestamp'],'day2' => ['$dayOfYear' => '$timestamp'],'hour' => ['$hour' => '$timestamp'],'minute_interval' => ['$subtract' => [['$minute' => '$timestamp'],['$mod' => [['$minute' => '$timestamp'], 1]]]]],'TopAsk' => ['$min' => '$ASK'],'TopBid' => ['$max' => '$BID'],'timestamp' => ['$first' => '$timestamp']]],
+    ['$project' => ['_id' => 0,'TopAsk' => 1,'TopBid' => 1,'ASK' => 1,'BID' => 1,'FairV' => ['$divide' => [['$add' => ['$TopAsk', '$TopBid']], 2]],'timestamp' => 1,'tz' => 1]],
+    ['$sort' => ['timestamp' => -1]],
+    ['$limit' => 500]
+  ]
+]);
 $cursor = $manager->executeCommand('bitfinex_ticker',$query);
 
 var_dump($result);
